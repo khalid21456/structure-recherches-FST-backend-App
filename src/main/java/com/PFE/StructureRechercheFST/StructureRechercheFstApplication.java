e36dc94f134fc6028ca736ce5f2b19d9e5633421
khalid21456 <khalidedaoudi8@gmail.com>
2024-04-22 12:30:01 +0100

    init

---
+++ src/main/java/com/PFE/StructureRechercheFST/StructureRechercheFstApplication.java
@@ -0,0 +1,13 @@
+package com.PFE.StructureRechercheFST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructureRechercheFstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructureRechercheFstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PFE/StructureRechercheFST/StructureRechercheFstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PFE.StructureRechercheFST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructureRechercheFstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

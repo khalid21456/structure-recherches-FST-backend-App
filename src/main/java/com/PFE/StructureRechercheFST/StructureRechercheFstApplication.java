66b6b3b7d8336cd82ef689655813d7e6b2a32d0b
khalid21456 <khalidedaoudi8@gmail.com>
2024-04-23 15:15:10 +0100

    add Doctorant entity

--- src/main/java/com/PFE/StructureRechercheFST/StructureRechercheFstApplication.java
+++ src/main/java/com/PFE/StructureRechercheFST/StructureRechercheFstApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StructureRechercheFstApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StructureRechercheFstApplication.class, args);
+
 	}
 
 }

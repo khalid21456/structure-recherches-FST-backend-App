4053d6bcb92b46fb2503b2f81aca1250719c65d9
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2024-05-15 15:07:04 +0100

    Update

--- src/main/java/com/PFE/StructureRechercheFST/Services/Enseignant/EnseignantService.java
+++ src/main/java/com/PFE/StructureRechercheFST/Services/Enseignant/EnseignantService.java
@@ -7,6 +7,8 @@ import com.PFE.StructureRechercheFST.models.Publication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 @SuppressWarnings("unused")
 public class EnseignantService {
@@ -22,6 +24,7 @@ public class EnseignantService {
         if(enseignantDAO.findById(idPublier).isPresent()) {
             enseignant = enseignantDAO.findById(idPublier).get();
         }
+        publication.setDatePub(new Date());
         publication.setEnseignant(enseignant);
         publicationDAO.save(publication);
     }
---
+++ src/main/java/com/PFE/StructureRechercheFST/Services/Enseignant/PublicationService.java
@@ -0,0 +1,11 @@
+package com.PFE.StructureRechercheFST.Services.Enseignant;
+
+import com.PFE.StructureRechercheFST.DAO.PublicationDAO;
+import com.PFE.StructureRechercheFST.models.Publication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PublicationService {
+
+}
